601145f935696d0096c9a1789144a2d09534a306
大塚　竜太郎 <otsukaryutaro@team-lab.com>
2024-01-29 20:22:32 +0900

    命名

--- src/pages/hoge/index.tsx
+++ src/pages/hoge/index.tsx
@@ -1,28 +1,48 @@
 import { DomPurifyComponent } from '@/components/dom-purify';
 import { SanitizeHtml } from '@/components/sanitize-html';
-import { useEffect } from 'react';
+import Link from 'next/link';
+import { useEffect, useRef } from 'react';
 
 export default function Home() {
+  // 初期表示が成功したかをフラグで管理
+  const flag = useRef(false);
+
   useEffect(() => {
     const currentPath = window.location.pathname;
-    sessionStorage.setItem("currentPage",currentPath); 
+    const storedPath = localStorage.getItem("storedPath");
+    
+    if(storedPath === currentPath) {
+      throw new Error("Same page");
+    }
+    
+    localStorage.setItem("storedPath",currentPath); 
+
+    // 初期表示が成功
+    flag.current = true;
+    
+    return () => {
+      localStorage.removeItem("storedPath");
+    }
   } ,[])
-
-  useEffect(() => { 
-    const onStorageChange = (e: StorageEvent) => {
-      console.log({e})
-    };
-
-    window.addEventListener("storage", onStorageChange);
-    return () => { 
-      window.removeEventListener("storage", onStorageChange);
-    };
-  },[]);
-
+  
+  useEffect(() => {
+    const handler = () => {
+      // リロードやタブを閉じるときに実行される
+      // ただし、初期表示で失敗した場合は実行されない
+      if(flag.current) {
+            localStorage.removeItem("storedPath");
+        }
+    }
+    window.addEventListener('beforeunload', handler);
+    return () => {
+      window.removeEventListener('beforeunload', handler);
+    }
+  }, []);
 
   return (
     <>
       <h1>This is /hoge/index.tsx</h1>
+      <Link href="/"><button type='button'>HOME</button></Link>
       <main
         style={{ backgroundColor: 'white', color: 'black', paddingLeft: 50 }}
       >
